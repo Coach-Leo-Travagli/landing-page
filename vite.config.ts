bca33aca9b427f2e7e2f3768060f186b6a12956d
Helder Traci <helderdiin.work@gmail.com>
2025-07-31 19:05:38 -0300

    feat: update vite.config.ts to include base and historyApiFallback for improved routing

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,9 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/', // importante para roteamento
   server: {
+    historyApiFallback: true,
     host: "::",
     port: 5173,
   },
