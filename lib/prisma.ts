312c3a0154dada8f959a9b8e544b0bf1f1354518
Helder Traci <helderdiin.work@gmail.com>
2025-08-04 16:53:38 -0300

    feat: add customer name to payments and create payments table in database

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,7 @@
 import { PrismaClient } from '@prisma/client';
 
+// PrismaClient is attached to the `global` object in development to prevent
+// exhausting your database connection limit.
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
