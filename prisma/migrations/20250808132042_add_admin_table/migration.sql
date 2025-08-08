18a3eaec8ccfcda6d79df3764ff0cf67ea81efe4
Helder Traci <helderdiin.work@gmail.com>
2025-08-08 10:30:04 -0300

    feat: added admin table and passwordHash column to users table

---
+++ prisma/migrations/20250808132042_add_admin_table/migration.sql
@@ -0,0 +1,17 @@
+-- AlterTable
+ALTER TABLE "public"."users" ADD COLUMN     "passwordHash" TEXT;
+
+-- CreateTable
+CREATE TABLE "public"."admins" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT,
+    "passwordHash" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "admins_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "admins_email_key" ON "public"."admins"("email");
